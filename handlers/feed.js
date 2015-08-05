1b9a2830a4c9aa7c87e6ea422baeca686c266ed7
cliftonc <clifton.cunningham@gmail.com>
2015-08-05 06:10:38 +0100

    Add debugging for travis

--- handlers/feed.js
+++ handlers/feed.js
@@ -8,12 +8,14 @@ module.exports = function (api, config) {
     async.parallel([
       async.apply(model.addUser, msg.user),
       async.apply(model.clearNotifications, msg.user)
-    ], function () {
-      //
+    ], function (err, result) {
+      console.dir(err);
+      console.dir(result);
     });
   };
 
   var add = function (msg) {
+    console.dir(msg);
     async.parallel([
       async.apply(model.addItem, msg.item, msg.data),
       async.apply(model.addNotification, msg.user, msg.item)
